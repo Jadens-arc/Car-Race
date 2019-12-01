b5abb0f3d30a6d7993ea0ea225e937f86fb52273
Jaden <mining.mojo@icloud.com>
2019-11-30 22:21:52 -0800

    Create Formula1.py

---
+++ Formula1.py
@@ -0,0 +1,18 @@
+import Race
+formula1 = Race(100)
+
+formula1.addCar('McLaren')
+formula1.addCar('Ferrari')
+formula1.addCar('Lamborghini')
+formula1.addCar('Pagani')
+formula1.addCar('Ford')
+formula1.addCar('Porche')
+formula1.addCar('Chevy')
+formula1.addCar('Dodge')
+formula1.addCar('Tesla')
+formula1.addCar('Renult')
+formula1.addCar('Hummer')
+formula1.addCar('Senna')
+formula1.addCar('Toyota')
+
+formula1.start()
